e389e34625a21c6b1d9b5e1973fed218143ff968
joao4xz <joao.4xz@gmail.com>
2024-03-19 17:28:24 -0300

    feat: add initial home page structure

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Initial Page</h1>
+    <div className="flex ">
+      <div>
+        <h2>"Se poluição é dinheiro, qual é o valor de um bom ar?"</h2>
+        <p>
+          Para iniciar o processo de compra e venda de carbono, é necessário
+          conectar sua carteira.
+        </p>
+      </div>
+      <div>
+        <img src="icon.png" alt="CO2" />
+        <h2>Bem-vindo novamente!</h2>
+        <p>Login com seu e-mail</p>
+        <button>Conecte sua carteira</button>
+      </div>
     </div>
   );
 }
